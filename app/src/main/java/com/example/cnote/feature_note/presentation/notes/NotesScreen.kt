88e1d1d5ccc5c4e2ef681c3312c9c69ea58dbc3d
amirhoseinb74 <ah.baratali74@gmail.com>
2024-04-25 07:31:29 +0330

    fix:bug- snackbar stays on the NotesScreen

--- app/src/main/java/com/example/cnote/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/example/cnote/feature_note/presentation/notes/NotesScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
@@ -80,7 +81,8 @@ fun NotesScreen(
                         scope.launch {
                             val result = snackbarHostState.showSnackbar(
                                 message = "Note deleted",
-                                actionLabel = "Undo"
+                                actionLabel = "Undo",
+                                duration = SnackbarDuration.Short
                             )
                             if (result == SnackbarResult.ActionPerformed) {
                                 viewModel.onEvent(NotesEvent.RestoreNote)
